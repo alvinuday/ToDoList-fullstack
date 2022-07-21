c849ac42e6126519f1238ef00bec9d4cad708980
Alvin Uday <udayalvin@gmail.com>
2022-07-21 23:07:36 +0530

    changes: backend

--- todolist_client/todolist_backend/server.js
+++ todolist_client/todolist_backend/server.js
@@ -12,14 +12,14 @@ app.post("/login", (req, res) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, "./todolist_client/build")));
-app.get("*", function (_, res) {
-  res.sendFile(path.join(__dirname, "./todolist_client/build/index.html")),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    };
-});
+// app.use(express.static(path.join(__dirname, "./todolist_client/build")));
+// app.get("*", function (_, res) {
+//   res.sendFile(path.join(__dirname, "./todolist_client/build/index.html")),
+//     function (err) {
+//       if (err) {
+//         res.status(500).send(err);
+//       }
+//     };
+// });
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server is running on PORT ${port}`));
